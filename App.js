2cf77c21db40bea64fa3a386e64183515fa40eb8
Lucas23-proyects <37665744+Lucas23-proyects@users.noreply.github.com>
2020-05-17 20:20:20 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
+import './App.css';
+import Inicio from './Inicio';
+
+function App() {
+  return (
+    <div className="App">
+     <Inicio></Inicio>
+    </div>
+  );
+}
+
+export default App;
