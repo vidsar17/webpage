55de06bf6b8a383b22208ba844b4404aeb7c5547
Lucas Caniella <canieluc@essenla.com>
2020-07-10 01:03:45 -0300

    ok

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { render } from 'react-dom';
 import './style/main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from  '../client/login/Login';
 
 const App = () => {
-    return <h1>Es un buen comienzo para el nuevo e-Ludum!!</h1>
+    return <Login></Login>
 }
 render(
     <App/>,
     document.getElementById('app')
 );
+
+
