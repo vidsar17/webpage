89042438195d1e91086263fbee9f27517ed1a187
Lucas Caniella <canieluc@essenla.com>
2020-07-10 01:04:19 -0300

    ok

---
+++ src/client/login/Login.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react'
+//import './card.css'
+import { Button,InputGroup,FormControl } from 'react-bootstrap';
+import { Component } from 'react';
+
+function Login() {
+
+    <div>
+         <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1">Usuario</InputGroup.Text>
+            </InputGroup.Prepend>
+            
+        </InputGroup> 
+        <Button  variant="dark">Guardar</Button>
+
+    </div>
+
+
+}
+
+export default Login
