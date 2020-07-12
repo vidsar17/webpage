926e7201d4c99548a68d5f2199b17c03c451a5d9
Angelito <vangel338@gmail.com>
2020-07-12 09:53:44 -0300

    cambios 2

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
-const db_pool = require('../dbConection'); 
-//const Component = require('../client/index.js');
+const db_pool = require('../dbConection');
+const sendMail = require('../mails/checkMail')
+    //const Component = require('../client/index.js');
 
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
@@ -9,15 +10,17 @@ const ReactDOMServer = require('react-dom/server');
 const Component = require('../client/component/login/Login')
 
 router.get('/', (req, res) => {
-    
+
     var html = ReactDOMServer.renderToString(
         React.createElement(Component)
     );
 
+    sendMail.configShipping();
     res.send(html);
+
 });
 
-router.get('/Api', async (req, res) => {
+router.get('/Api', async(req, res) => {
 
     const user = await db_pool.query(`select * from eludumdb.usuarios`);
 
