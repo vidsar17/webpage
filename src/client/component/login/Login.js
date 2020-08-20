10e0c6f9a2f24922ae9b240db249987c3e045c91
81helpAngel <anvargas@grupoassa.com>
2020-08-20 00:47:53 -0300

    arreglos

--- src/client/component/login/Login.js
+++ src/client/component/login/Login.js
@@ -17,7 +17,6 @@ class App extends React.Component {
         this.updateusername = this.updateusername.bind(this);
         this.updatepassword = this.updatepassword.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleSubmitprueba = this.handleSubmitprueba.bind(this);
     }
 
     updateusername(event) {
@@ -32,40 +31,6 @@ class App extends React.Component {
             user: this.state.username,
             pass: this.state.password
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
-        handleSubmit(){
-           const data = { 
-                user: this.state.username,
-                pass: this.state.password
-            }  
-
-            try {
-                let config = {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(data)
-                }
-
-                fetch('http://localhost:3302/getUser', config)
-                    .then(res => res.json())
-                    .then((data) => {
-                        if(data[0].error == 'Error'){ alert('Usuario o Contraseña incorrectos') }
-                        if(data[0].error == 'Ok'){ alert('BIENVENIDO')
-
-
-                     }
-                    });
-
-            } catch (error){
-                if(error){console.log('Error: ', error)}
-=======
-=======
->>>>>>> abfeedd9faa4f5acaf928bfd9af57e9e4c944a93
 
         try {
             let config = {
@@ -75,10 +40,6 @@ class App extends React.Component {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(data)
-<<<<<<< HEAD
->>>>>>> abfeedd9faa4f5acaf928bfd9af57e9e4c944a93
-=======
->>>>>>> abfeedd9faa4f5acaf928bfd9af57e9e4c944a93
             }
 
             fetch('http://localhost:3302/getUser', config)
@@ -91,86 +52,12 @@ class App extends React.Component {
         } catch (error) {
             if (error) { console.log('Error: ', error) }
         }
-        handleSubmitprueba(){
-            const data = { 
-                 user: this.state.username,
-                 pass: this.state.password
-             }  
- 
-             try {
-                 let config = {
-                     method: 'POST',
-                     headers: {
-                         'Accept': 'application/json',
-                         'Content-Type': 'application/json'
-                     },
-                     body: JSON.stringify(data)
-                 }
- 
-                 fetch('http://localhost:3302/getUserprueba', config)
-                     .then(res => res.json())
-                     .then((data) => {
-                         if(data[0].error == 'Error'){ alert('Usuario o Contraseña incorrectos') }
-                         if(data[0].error == 'Ok'){ alert('BIENVENIDO')
- 
- 
-                      }
-                     });
- 
-             } catch (error){
-                 if(error){console.log('Error: ', error)}
-             }
- 
-         }
-
-    }
 
     }
 
     render() {
 
         return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-        
-       <div>
-         	<div className="modal-dialog login">
-		<div className="modal-content">
-			<div className="modal-header">
-            <div class="avatar">
-					<img src={'../component/imgs/login.jpg'} alt="Avatar"></img>
-				</div>
-					
-				<h4 class="modal-title">BIENVENIDO</h4>	
-               
-			</div>
-			<div className="modal-body">
-				
-					<div className="form-group">
-			
-                        <input type="email" className="form-control" id="username" placeholder="Usuario" required="required" onChange={this.updateusername}></input>	
-					</div>
-					<div class="form-group">
-						
-                       
-                        <input type="password" className="form-control" placeholder="Password" id="password" onChange={this.updatepassword}></input>
-					</div>        
-					<div class="form-group">
-						<button type="submit"   onClick={this.handleSubmit} className="btn btn-primary btn-lg btn-block login-btn">Ingresar</button>
-                        <button type="submit"   onClick={this.handleSubmitprueba} className="btn btn-primary btn-lg btn-block login-btn">Prueba</button>
-					</div>
-				
-			</div>
-			<div class="modal-footer">
-				<a href="#">Recuperar Password</a>
-			</div>
-		</div>
-	</div>
-       </div>
-		
-=======
-=======
->>>>>>> abfeedd9faa4f5acaf928bfd9af57e9e4c944a93
             <div>
                 <Container>
 
@@ -201,12 +88,8 @@ class App extends React.Component {
                         </InputGroup>
 
                         <Button type="submit" onClick={this.handleSubmit} variant="dark">Inicio</Button>
-<<<<<<< HEAD
->>>>>>> abfeedd9faa4f5acaf928bfd9af57e9e4c944a93
-=======
->>>>>>> abfeedd9faa4f5acaf928bfd9af57e9e4c944a93
 
-                        
+                
                     </Col>
                 </Container>
             </div>
